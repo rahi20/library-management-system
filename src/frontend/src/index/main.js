36a5a8ea46ed54159021b4937c85de7fcb9fcd53
Rahima Largo <largorahima@gmail.com>
2022-01-14 16:43:00 +0100

    deleted unnecessary lines

--- src/frontend/src/index/main.js
+++ src/frontend/src/index/main.js
@@ -1,16 +1,6 @@
-//import * as Vue from 'vue'
-import Vue from "vue";
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-import { faUser } from '@fortawesome/free-solid-svg-icons'
-import { faKey } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faUser, faKey)
-
 createApp(App)
-    .component("font-awesome-icon", FontAwesomeIcon)
     .mount('#app')
 
